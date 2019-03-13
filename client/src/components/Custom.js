2ffc7a4983315ac5389ef6cee586baa857dc5aff
mochiyuki <mochiyuki@users.noreply.github.com>
2019-03-13 03:49:03 +0100

    delete test files

--- client/src/components/Custom.js
+++
@@ -1,9 +0,0 @@
-const AFRAME = window.AFRAME;
-//const THREE = require("aframe/src/lib/three");
-
-AFRAME.registerComponent("wireframe", {
-  dependencies: ["material"],
-  init: function() {
-    this.el.components.material.material.wireframe = true;
-  }
-});
